528bb580df367562085d089e992f972c3eb2d74a
Melissa Williams <melissa@brandmeetscopy.com>
2019-03-14 18:34:27 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,13 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz
+def fizzbuzz(n)
+  if n % 3 == 0
+    puts "Fizz"
+  elsif n % 5 == 0
+    puts "Buzz"
+  elsif n % 15 == 0
+    puts "FizzBuzz"
+  else
+    puts n
 
 end
